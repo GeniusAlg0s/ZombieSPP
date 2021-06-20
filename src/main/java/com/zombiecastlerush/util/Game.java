3b783f472118674ec45ca64e1d457870f05c0c70
pasang2044 <pasang.sherpa2044@gmail.com>
2021-06-20 11:21:30 -0700

    The user can gather coin and use wand, potion as needed to defeat the enemy. AB#533

--- src/main/java/com/zombiecastlerush/util/Game.java
+++ src/main/java/com/zombiecastlerush/util/Game.java
@@ -34,7 +34,6 @@ public class Game {
         String userName = Prompter.getUserInput("Welcome to Zombie Castle Rush! \n\nPlease enter your name:");
         player = new Player(userName);
         player.setCurrentPosition(castle.getCastleRooms().get("Castle-Hall"));
-        player.setAcctBalance(500);
         showInstructions();
 
         while (true) {
